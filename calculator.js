df95daf5038d5f6f7d37bf2519463b011917e76b
poonamO7 <o7services020@gmail.com>
2020-11-03 04:17:21 +0530

    WIP show results along with log values in single input field

--- calculator.js
+++ calculator.js
@@ -3,9 +3,11 @@ const addBtn = document.getElementById('addBtn');
 const subtractBtn = document.getElementById('subtractBtn');
 const multiplyBtn = document.getElementById('multiplyBtn');
 const divideBtn = document.getElementById('divideBtn');
+const btnModule = document.getElementById('btnModule');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
+const btnReset = document.getElementById('btnReset');
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
@@ -31,7 +33,8 @@ function show(val) {
 // Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription); // from vendor file
+  // outputResult(currentResult, calcDescription);
+  usrInput.value = calcDescription;
 }
 
 function writeToLog(
@@ -82,7 +85,26 @@ function divide() {
   writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
 
+function module() {
+  const enteredNumber =getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult %= enteredNumber;
+  createAndWriteOutput('%', initialResult, enteredNumber);
+  writeToLog('MODULE', initialResult, enteredNumber, currentResult);
+}
+
+function displayResult() {
+  usrInput.value = currentResult;
+}
+
+function clearAll() {
+  usrInput.value = "";
+}
+
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
+
+equalToBtn.addEventListener('click', displayResult);
+btnReset.addEventListener('click', clearAll);
